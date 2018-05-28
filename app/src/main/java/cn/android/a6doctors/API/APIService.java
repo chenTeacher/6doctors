252101e81e286fd7317c47231509e411d487de74
ChenTeacher <343542102@qq.com>
2018-05-28 23:58:42 +0800

    ...

--- app/src/main/java/cn/android/a6doctors/API/APIService.java
+++ app/src/main/java/cn/android/a6doctors/API/APIService.java
@@ -1,19 +1,51 @@
 package cn.android.a6doctors.API;
 
+import java.util.Map;
+
+import okhttp3.MultipartBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 
 /**
  * Created by ChenTeacher on 2018-05-15.
  */
 
 public interface APIService {
-        @GET("service/getPatientList.do?")
-        Call<ResponseBody> getPatientList(@Query("doctorId") String doctorId);
+        /**
+         * 登录app
+         * @param account 账号
+         * @param password 密码
+         * @return
+         */
+        @FormUrlEncoded
+        @POST("api/login?")
+        Call<ResponseBody> login(@Field("account") String account,
+                                 @Field("password") String password);
 
-        @GET("service/getPatientInfo.do?")
-        Call<ResponseBody> getPatientInfo(@Query("doctorId") String doctorId);
+        /**
+         * 查询所有的患者
+         * @param token
+         * @param doctorId 医生的id
+         * @return
+         */
+        @GET("api/getPatients?")
+        Call<ResponseBody> getPatients(@Query("token")String token,
+                                      @Query("doctorId") int doctorId);
+        @Multipart
+        @POST("api/createPatient?")
+        Call<ResponseBody> createPatient(@QueryMap Map<String,Object> params, @Part MultipartBody.Part file);
+//        @GET("api/getPatientList.do?")
+//        Call<ResponseBody> getPatientList(@Query("doctorId") String doctorId);
+//
+//        @GET("service/getPatientInfo.do?")
+//        Call<ResponseBody> getPatientInfo(@Query("doctorId") String doctorId);
 
 }
